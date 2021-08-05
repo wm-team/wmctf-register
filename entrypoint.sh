a6d42b5c2df2b253b781b8fad66b0a1c2fbeb54c
Yoshino-s <yoshino.prog@gmail.com>
2021-08-05 16:25:48 +0800

    fix socat config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cd /app && socat -T60 TCP4-LISTEN:8000,reuseaddr,fork EXEC:"/usr/bin/env python -u app.py",pty,stderr
+cd /app && socat -T60 TCP4-LISTEN:8000,reuseaddr,fork EXEC:"/usr/bin/env python -u app.py",stderr
