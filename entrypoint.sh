33348c41cb4d99c6bba6d069c67b4eb6a4a5e84c
Yoshino-s <yoshino.prog@gmail.com>
2023-07-30 21:18:35 +0800

    fix: env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-env | grep _ >> /etc/environment
+env | egrep -v "^(HOME=|USER=|MAIL=|LC_ALL=|LS_COLORS=|LANG=|HOSTNAME=|PWD=|TERM=|SHLVL=|LANGUAGE=|_=)" >> /etc/environment
 /usr/sbin/sshd -D -e
